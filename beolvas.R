7986bc47149f87f0596052707676c0e6d8b7cf03
Péter Kalicz <kaliczp@gmail.com>
2023-05-23 14:06:49 +0200

    Note to data sources

--- beolvas.R
+++ beolvas.R
@@ -1,4 +1,7 @@
+## Követelmény: legyen egy mappa/mappák, amiben benne vannak a feldolgozandó fájlok.
+## Itt a nevük konvertalt2022 és konvertalt2023
 ## Fájl lista létrehozás
 ttev  <- 2022
 ttmappanev <- paste0("konvertalt", ttev, "/")
 txtfiles <- dir(ttmappanev, pattern = "TXT")
+
