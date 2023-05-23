b76c4a12f9cb962e3c3c8d0ec0183e204477d1cc
Péter Kalicz <kaliczp@gmail.com>
2023-05-23 14:41:20 +0200

    Read in next year based on previous lines

--- beolvas.R
+++ beolvas.R
@@ -14,3 +14,10 @@ nedvesret <- smartbe(paste0(ttmappanev, txtfiles[1]), channel = 1)
 for(ttfile in txtfiles[-1]) {
     nedvesret <- rbind(nedvesret, smartbe(paste0(ttmappanev, ttfile), channel = 1))
 }
+## Next year
+ttev  <- 2023
+ttmappanev <- paste0("konvertalt", ttev, "/")
+txtfiles <- dir(ttmappanev, pattern = "TXT")
+for(ttfile in txtfiles) {
+    nedvesret <- rbind(nedvesret, smartbe(paste0(ttmappanev, ttfile), channel = 1))
+}
